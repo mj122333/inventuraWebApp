665ecbbbbfbad11f4aad9b07db42a3ef180b0435
vitolist <vito.list2005@gmail.com>
2023-06-13 23:01:46 +0200

    generiranje barcode png iz baze

--- barcode_generator.py
+++ barcode_generator.py
@@ -1,9 +1,30 @@
 from barcode import EAN13
 from barcode.writer import ImageWriter
 from random import randint
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
 
-for i in range(1):
-    number = randint(1000000000000, 9999999999999)
-    code = EAN13(str(number), writer=ImageWriter())
+db = mysql.connector.connect(
+    host="localhost",
+    user="vito",
+    password="micko",
+    database="inventura"
+)
 
-    code.save("new_code{}".format(i))
+cursor = db.cursor()
+
+sql = "SELECT * FROM test"
+cursor.execute(sql)
+result = cursor.fetchall()
+column_names = cursor.column_names
+
+for row in result:
+    ID = row[column_names.index('id')]
+    code = EAN13(ID, writer=ImageWriter())
+    code.save("code_{}".format(ID))
+
+db.commit()
+
+cursor.close()
+db.close()
