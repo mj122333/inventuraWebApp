b864a928afac8eca6af8e35958da1e441ec36f84
vitolist <vito.list2005@gmail.com>
2023-09-03 01:14:10 +0200

    updated config folder

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+define('DB_SERVERNAME', 'localhost');
+define('DB_USERNAME', 'vito');
+define('DB_PASSWORD', 'micko');
+define('DB_NAME', 'inventura');
